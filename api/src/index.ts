6696be135a134cdddb3177551d8ce10f72daa408
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-15 22:51:45 -0300

    chore: configure json parser and statics files

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,14 @@
+import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './router.js';
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
+    app.use(express.json());
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(router);
     app.listen(3001);
   })
   .catch(() => {
