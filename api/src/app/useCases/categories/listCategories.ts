f4c41db384f7def6b2431abc066a6b7642adde93
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-15 22:40:05 -0300

    feat: list categories use case

---
+++ api/src/app/useCases/categories/listCategories.ts
@@ -0,0 +1,11 @@
+import type { Request, Response } from 'express';
+import { Category } from '../../models/Category.js';
+
+export async function listCategories(request: Request, response: Response) {
+  try {
+    const categories = await Category.find();
+    response.json({ categories });
+  } catch {
+    response.sendStatus(500);
+  }
+}
