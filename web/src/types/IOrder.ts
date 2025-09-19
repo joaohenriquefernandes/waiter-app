508c60149337aa6dcad72ddd18aff17862b8698d
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-18 23:38:31 -0300

    fix: change type of order interface

--- web/src/types/IOrder.ts
+++ web/src/types/IOrder.ts
@@ -1,7 +1,7 @@
 export interface IOrder {
   _id: string;
   table: string;
-  status: string;
+  status: 'WAITING' | 'IN_PRODUCTION' | 'DONE';
   products: {
     _id: string;
     quantity: number;
@@ -9,6 +9,6 @@ export interface IOrder {
       name: string;
       imagePath: string;
       price: number;
-    }[]
+    }
   }[]
 }
