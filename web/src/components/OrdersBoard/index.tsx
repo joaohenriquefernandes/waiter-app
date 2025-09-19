6b6da1621052cbac047538a30e10634a5fc1d889
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-18 23:41:49 -0300

    feat: edit order board component

--- web/src/components/OrdersBoard/index.tsx
+++ web/src/components/OrdersBoard/index.tsx
@@ -1,5 +1,7 @@
-import type { IOrder } from '../../types/Order';
+import { useState } from 'react';
+import type { IOrder } from '../../types/IOrder';
 import { Board, OrdersContainer } from './styles';
+import { OrderModal } from '../OrderModal';
 
 interface IOrdersBoardProps {
   icon: string;
@@ -7,29 +9,43 @@ interface IOrdersBoardProps {
   orders: IOrder[]
 }
 
-export function OrdersBoard({ icon, title }: IOrdersBoardProps) {
+export function OrdersBoard({ icon, title, orders }: IOrdersBoardProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<IOrder | null>(null);
+
+  function handleOpenModal(order: IOrder) {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  }
+
+  function handleCloseModal() {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
+  }
+
   return (
     <Board>
+      <OrderModal
+        visible={isModalVisible}
+        order={selectedOrder}
+        onClose={handleCloseModal}
+      />
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
-        <span>(1)</span>
+        <span>({orders.length})</span>
       </header>
 
-      <OrdersContainer>
-        <button type='button'>
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-        <button type='button'>
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-        <button type='button'>
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-      </OrdersContainer>
+      {orders.length > 0 && (
+        <OrdersContainer>
+          {orders.map((order) => (
+            <button type='button' key={order._id} onClick={() => handleOpenModal(order)}>
+              <strong>Mesa {order.table}</strong>
+              <span>{order.products.length} itens</span>
+            </button>
+          ))}
+        </OrdersContainer>
+      )}
     </Board>
   );
 }
