221349eea3e4d26756211ac9b5b86d9ad1778403
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-21 20:59:54 -0300

    feat: create table modal component

---
+++ mobile/src/components/TableModal/index.tsx
@@ -0,0 +1,53 @@
+import { Modal, Platform, TouchableOpacity } from 'react-native';
+import { Text } from '../Text';
+import { Form, Header, Input, ModalBody, Overlay } from './styles';
+import { Close } from '../Icons/Close';
+import { Button } from '../Button';
+import { useState } from 'react';
+
+interface ITableModalProps {
+  visible: boolean;
+  onClose: () => void;
+  onSave: (table: string) => void;
+}
+
+export function TableModal({visible, onClose, onSave }: ITableModalProps) {
+  const [table, setTable] = useState('0');
+  const isAndroid = Platform.OS === 'android';
+  function handleSave() {
+    onSave(table);
+    onClose();
+  }
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType='fade'
+    >
+      <Overlay behavior={isAndroid ? 'height' : 'padding'}>
+        <ModalBody>
+          <Header>
+            <Text weight='600'>Informe a mesa</Text>
+            <TouchableOpacity onPress={onClose}>
+              <Close color='#666666'/>
+            </TouchableOpacity>
+          </Header>
+          <Form>
+            <Input
+              placeholder='Número da mesa'
+              placeholderTextColor='#666666'
+              keyboardType='number-pad'
+              onChangeText={setTable}
+            />
+            <Button
+              onPress={handleSave}
+              disabled={table.length === 0}
+            >
+              Salvar
+            </Button>
+          </Form>
+        </ModalBody>
+      </Overlay>
+    </Modal>
+  );
+}
--- mobile/src/main/index.tsx
+++ mobile/src/main/index.tsx
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
+import { TableModal } from '../components/TableModal';
 import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from './styles';
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -17,9 +26,19 @@ export function Main() {
       </Container>
       <Footer>
         <FooterContainer>
-
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
