61a47147aaebf514c101bc155963c402410af1ea
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-21 20:05:26 -0300

    feat: create menu component

---
+++ mobile/src/components/Menu/index.tsx
@@ -0,0 +1,43 @@
+import { FlatList } from 'react-native';
+import { Text } from '../Text';
+import { Product, ProductDetails, ProductImage, Separator, AddToCartButton } from './styles';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+
+export function Menu() {
+  const products: any[] = [];
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={product => product._id}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      ItemSeparatorComponent={Separator}
+      renderItem={({item}) => (
+        <Product>
+          <ProductImage
+          source={{
+            uri: `exp://192.168.0.115:3001/uploads/${item.imagePath}`,
+          }}
+          />
+          <ProductDetails>
+            <Text weight='600'>{item.name}</Text>
+            <Text
+              color='#666666'
+              size={14}
+              style={{ marginVertical:8 }}
+            >
+              {item.description}
+            </Text>
+            <Text size={14} weight='600'>
+              {formatCurrency(item.price)}
+              </Text>
+          </ProductDetails>
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
+        </Product>
+      )}
+    />
+  );
+}
