977f4d8540d376be5b38b1f8f968a429c88d0c1b
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-21 16:18:46 -0300

    feat: create new styles to categories component

--- mobile/src/components/Categories/index.tsx
+++ mobile/src/components/Categories/index.tsx
@@ -1,9 +1,17 @@
 import { FlatList } from 'react-native';
 import { Text } from '../Text';
 import { Category, Icon } from './styles';
+import { useState } from 'react';
 
 export function Categories() {
   const categories: any[] = [];
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
   return (
     <FlatList
       data={categories}
@@ -11,15 +19,21 @@ export function Categories() {
       showsHorizontalScrollIndicator={false}
       horizontal
       contentContainerStyle={{ paddingRight: 24 }}
-
-      renderItem={({item}) => (
-        <Category>
-          <Icon>
-            <Text size={14} weight='600'>{item.icon}</Text>
-          </Icon>
-          <Text size={14} weight='600'>{item.name}</Text>
-        </Category>
-      )}
+      renderItem={({item}) => {
+        const isSelected = selectedCategory === item._id;
+        return (
+          <Category onPress={() => handleSelectCategory(item._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>
+                {item.icon}
+              </Text>
+            </Icon>
+            <Text size={14} weight='600'>
+              {item.name}
+            </Text>
+          </Category>
+        );
+      }}
     />
   );
 }
