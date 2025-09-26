2ef5920c573fda4a9e76fbd6a053d3db990401e7
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-26 00:23:16 -0300

    chore: configure cors and web socket protocol

--- api/src/app/useCases/orders/createOrder.ts
+++ api/src/app/useCases/orders/createOrder.ts
@@ -1,10 +1,13 @@
 import type { Request, Response } from 'express';
 import { Order } from '../../models/Order.js';
+import { io } from '../../../index.js';
 
 export async function createOrder(request: Request, response: Response) {
   try {
     const { table, products } = request.body;
     const order = await Order.create({ table, products });
+    const orderDetails = await order.populate('products.product');
+    io.emit('orders@new', orderDetails);
     response.status(201).json({ order });
   } catch {
     response.sendStatus(500);
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,26 @@
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
 import { router } from './router.js';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
+    app.use((request, response, next) => {
+      response.setHeader('Access-Control-Allow-Origin', 'http://192.168.0.115:5173');
+      response.setHeader('Access-Control-Allow-Methods', '*');
+      response.setHeader('Access-Control-Allow-Headers', '*');
+      next();
+    });
     app.use(express.json());
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(router);
-    app.listen(3001);
+    server.listen(3001);
   })
   .catch(() => {
     console.log('Error');
